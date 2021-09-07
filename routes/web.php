d2425f0d76d0fd54c07b4992428a92fd1cde58f5
takumi1225 <takumi.a.tani@rakuten-sec.co.jp>
2021-09-07 13:57:20 +0900

    3-7

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@ Route::get('XXX', 'AAAController@bbb');
  //4.【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、getメソッドでadmin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
 
 Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    
     Route::get('profile/create', 'Admin\ProfileController@add');
-});
-
-Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+
