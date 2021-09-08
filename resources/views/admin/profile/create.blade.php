3293a4083231baf8df1c914bcaa5bc32f9feabe0
takumi1225 <takumi.a.tani@rakuten-sec.co.jp>
2021-09-08 19:14:50 +0900

    3-9

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,41 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.profile')
+@extends('layouts.admin')
+@section('title', '自己紹介欄(introduction)')
 
-
-{{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
-@section('title', 'Myプロフィール')
-
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>Myプロフィール</h2>
+                <h2>自己紹介欄(introduction)</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名(name)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">性別(gender)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">趣味(hobby</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="送信">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,13 @@ Route::get('XXX', 'AAAController@bbb');
 
  //4.【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、getメソッドでadmin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    
+    Route::post('news/create', 'Admin\NewsController@create');
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');;
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 
